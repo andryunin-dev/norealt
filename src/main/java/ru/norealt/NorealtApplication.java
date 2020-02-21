800236a285399697cb4e7c81daacf662cd6df26e
Andrey <an>
2020-02-21 20:30:39 +0400

    initial commit

---
+++ src/main/java/ru/norealt/NorealtApplication.java
@@ -0,0 +1,13 @@
+package ru.norealt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorealtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NorealtApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ru/norealt/controller/GreetingController.java
@@ -0,0 +1,25 @@
+package ru.norealt.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.norealt.repo.MessageRepo;
+
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+
+
+    @Autowired
+    private MessageRepo messageRepo;
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
+                           Map<String, Object> model
+    ) {
+        model.put("name", name);
+        return "greeting";
+    }
+}
